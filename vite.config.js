e9b017d7033d57d97dd5a654641d8d1d2754d391
Jose Ortega <gilberto_qc@hotmail.com>
2025-11-03 14:26:23 -0700

    Ahora si

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            // ESTA ES LA LÍNEA QUE ARREGLAMOS:
+            // Le decimos que busque los archivos fuente en 'resources/'
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
